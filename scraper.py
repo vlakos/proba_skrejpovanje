a64ad6fa110659362e3d5a7f92ba5c7e901f9411
vlakos <vkostic86@gmail.com>
2019-02-10 09:25:17 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,22 +1,12 @@
-import urllib2
-from xml.dom.minidom import parseString
-
-def get_google_new_results( term, count ):
-    results = []
-    obj = parseString( urllib2.urlopen('http://news.google.com/news?q=%s&output=rss' % term).read() )
-
-    elements = obj.getElementsByTagName('title')[2:] # To get rid of unwanted title elements in XML doc    
-    links = obj.getElementsByTagName('link')[2:]
-    print links
-    for element in elements[:count]:
-        headline =  element.childNodes[0].data
-        for link in links:
-            url = link.childNodes[0].data.split('=')[-1]
-        newssearch = headline + ' -> ' + url
-        results.append( newssearch )
-
-    return results
-
-items = get_google_new_results( 'CINS', 10 )
-for i,e in enumerate(items):
-    print '%d: %s' % (i+1,e,)
+import requests
+import lxml.html as lh
+import pandas as pd
+url='http://www.acas.rs/acasPublic/pretragaPrilogaLica.htm'
+#Create a handle, page, to handle the contents of the website
+page = requests.get(url)
+#Store the contents of the website under doc
+doc = lh.fromstring(page.content)
+#Parse data that are stored between <tr>..</tr> of HTML
+tr_elements = doc.xpath('//tr')
+#Check the length of the first 12 rows
+[len(T) for T in tr_elements[:12]]
